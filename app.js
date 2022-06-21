52116043eb61d63aea3e2c7f18af073b62203c67
Hunimoda <hunimoda@gmail.com>
2022-06-21 20:33:55 +0900

    handle /, /message request

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const http = require("http");
+const fs = require("fs");
+
 const server = http.createServer(
     (req, res) => {
-      // console.log(req.url, req.method, req.headers);
-      // process.exit();  // you don't want to write this
-                          // it stops your server
+      const url = req.url;
+      const method = req.method;
+
+      if (url === "/") {
+        res.write("<html>");
+        res.write("<head><title>Enter Message</title></head>");
+        res.write("<body><form action=\"message\" method=\"POST\"><input type=\"text\" name=\"message\" /><button type=\"submit\">Send</button></form></body>");
+        res.write("</html>");
+        return res.end();
+      } else if (url === "/message" && method === "POST") {
+        fs.writeFileSync("message.txt", "DUMMY");
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      }
       res.setHeader("Content-Type", "text/html");
       res.write("<html>");
       res.write("<head><title>My First Page</title></head>");
