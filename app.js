820d695220e57a6db1d438aa6bf605a276be66d6
Hunimoda <hunimoda@gmail.com>
2022-06-21 20:17:48 +0900

    work with request object and send back response

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const http = require("http");
-const server = http.createServer((req, res) => { console.log(req); });
+const server = http.createServer(
+    (req, res) => {
+      // console.log(req.url, req.method, req.headers);
+      // process.exit();  // you don't want to write this
+                          // it stops your server
+      res.setHeader("Content-Type", "text/html");
+      res.write("<html>");
+      res.write("<head><title>My First Page</title></head>");
+      res.write("<body><h1>Hello from Node.js</h1></body>");
+      res.write("</html>");
+      res.end();
+    });
 server.listen(3000);
