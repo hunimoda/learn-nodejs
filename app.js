785dabd94c2fac8326f414c572c5a5d5bdad9686
Hunimoda <hunimoda@gmail.com>
2022-06-23 22:00:32 +0900

    change to sendFile, route to corresponding html files

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const rootDir = require('./util/path');
 
 const app = express();
 
@@ -14,7 +16,7 @@ app.use(shopRoutes);
 
 // 404 page not found
 app.use((req, res, next) => {
-  res.status(404).send('<h1>404 page not found</h1>');
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,12 @@
+const path = require('path');
 const express = require('express');
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('index');
+  // path.join works any environment
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
