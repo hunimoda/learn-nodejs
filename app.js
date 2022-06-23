42800369558cea75f00d70bcc559a381afbdf8f3
Hunimoda <hunimoda@gmail.com>
2022-06-23 21:03:21 +0900

    handle 404 page not found

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 404 page not found
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 page not found</h1>');
+});
+
 app.listen(3000);
