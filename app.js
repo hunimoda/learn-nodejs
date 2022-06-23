4ce02decfa96912ce86c4ed6700cb28a4b3adb73
Hunimoda <hunimoda@gmail.com>
2022-06-23 20:45:28 +0900

    move routing logic into routes files

--- app.js
+++ app.js
@@ -3,20 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 // parse body
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (_, res) => {
-  res.send('<form action="/product", method="POST">' +
-           '<input type="text" name="title" />' + 
-           '<button type="submit">Add Product</button></form>');
-});
-app.post('/product', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-app.use('/', (req, res, next) => {
-  res.send('index');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
